2681b55e8b5d7874d85a0e2900ecb970b2d8d161
ShravanTale <shravanupa580@gmail.com>
2026-06-16 21:47:34 +0530

    Update Merger.js
    Fixed Download thing

--- Routers/Merger.js
+++ Routers/Merger.js
@@ -78,7 +78,7 @@ router.post("/", upload.array('pdfFiles', 10), async (req, res) => {
     fs.writeFileSync(`merged.pdf`, pdfBytes);
     //Sending url to the client to download the merged pdf
     try {
-        res.json({ downloadUrl: `${protocol}:/${host}/uploads/merged.pdf` });
+        res.json({ downloadUrl: 'https://pdf-merger-app-bxtk.onrender.com/uploads/merged.pdf' });
     }
     catch (err) {
         console.log("Error in sending file: ", err);
