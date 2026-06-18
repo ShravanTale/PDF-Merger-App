7e170a668ddb596ee50aa635dac1e2b7d0ebf5f1
ShravanTale <shravanupa580@gmail.com>
2026-06-18 23:20:08 +0530

    Update UploadFiles.js

--- Public/UploadFiles.js
+++ Public/UploadFiles.js
@@ -5,18 +5,24 @@ const label = document.querySelector('.file-label');
 const mergeBtn = document.getElementById('MergeBtn');
 const addIcon = document.querySelector('.add-icon');
 const DownloadBtn = document.getElementById('Download');
-const LoadingBar = document.getElementById('loadingSection');
+const LoadingCircle = document.getElementById('loadingSection');
+const Tagline = document.querySelector('.top')
+const percent = document.getElementById('percent');
 
 const formData = new FormData();
 
 // Function to fetch files and send to server
 async function fetchFiles(e) {
-  const UploadObjects = [mergeBtn, label, ListFiles];
-    for (let item of UploadObjects) {
-      item.style.display = 'none';
-    }
-  LoadingBar.style.display = "block";
-  mergeBtn.value = "Merging..."
+  LoadingCircle.style.display = 'block';
+  percent.innerText = "0%";
+  mergeBtn.value = "Merging...";
+  setTimeout(() => {
+    percent.innerText = "30%";
+  }, 500);
+
+  setTimeout(() => {
+    percent.innerText = "90%";
+  }, 5000);
 
   console.log("Merge button clicked and file lenght is: ", formData.getAll('pdfFiles').length);
   const options = {
@@ -27,8 +33,15 @@ async function fetchFiles(e) {
   const response = await fetch("/uploads", options);
   if (response.ok) {
     const blob = await response.blob();
-    LoadingBar.style.display = "none";
-     DownloadBtn.style.display = 'flex';
+    percent.innerText = "100%";
+
+    const UploadObjects = [mergeBtn, label, ListFiles];
+    for (let item of UploadObjects) {
+      item.style.display = 'none';
+    }
+    LoadingCircle.style.display = 'none';
+
+    DownloadBtn.style.display = 'flex';
     DownloadBtn.classList.add('flex-all');
 
     DownloadBtn.addEventListener('click', () => {
@@ -39,7 +52,7 @@ async function fetchFiles(e) {
       DownloadBtn.href = dynamicDownloadUrl;
       DownloadBtn.download = "your-merged-document.pdf";
 
-    }) 
+    })
 
   } else {
     console.error("Download Failed");
@@ -65,14 +78,16 @@ async function HandleFileSelect(e) {
   //Creating a list item to display the list of selected files on the webpage
   //Appending the name of the selected files to the list item via for loop
   //Appending the list item to the file list to print the list on the webpage
-  ListFiles.style.display = "block";
+  Tagline.style.display = 'none';
+  ListFiles.style.display = 'flex';
+  ListFiles.style.justifyContent = 'center';
+  ListFiles.classList.add('flex-dir-c');
   const listItem = document.createElement('li');
-  listItem.classList.add('flex');
-  listItem.classList.add('flex-dir-c');
-  listItem.style.fontFamily = 'Sans-Serif';
+  listItem.style.fontFamily = 'monospace';
   for (const file of pdffiles.files) {
     listItem.textContent += `${file.name}, `;
   }
+
   ListFiles.appendChild(listItem);
 
 
@@ -80,12 +95,14 @@ async function HandleFileSelect(e) {
   label.classList.add('material-symbols-outlined', 'add-icon');
   label.classList.remove('file-label');
   label.innerHTML = "add_circle";
+  label.style.marginBottom = "40px";
+
 
   //Checking if more than 1 file is selected to enable merge button
   if (formData.getAll('pdfFiles').length > 1) {
     //Enabling merge button
-    mergeBtn.style.backgroundColor = "black";
-    mergeBtn.style.color = "white";
+    mergeBtn.style.backgroundColor = "white";
+    mergeBtn.style.color = "black";
     mergeBtn.style.cursor = "pointer";
     //Adding event listener to merge button to trigger fetchFiles function on click
     mergeBtn.addEventListener('click', (e) => fetchFiles(e));
